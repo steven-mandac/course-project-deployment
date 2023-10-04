e6bd9b1ca37af2cf5ab171dab470cc95646a350a
Steven Lloyd Mandac <steven.mandac15@gmail.com>
2023-10-04 21:24:49 +0800

    uncomment load_dotenv function

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import smtplib
 import os
 
@@ -30,7 +30,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 
-# load_dotenv("./.env")
+load_dotenv("./.env")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
